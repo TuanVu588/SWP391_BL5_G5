ac7bdca2aa9cb3b40439426112539473336e5526
TuanVu588 <80246371+TuanVu588@users.noreply.github.com>
2023-04-17 10:12:50 +0700

    crud category

---
+++ SneakerOnlineShop/SneakerOnlineShop/Pages/Admin/Category/Delete.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SneakerOnlineShop.Models;
+
+namespace SneakerOnlineShop.Pages.Admin.Category
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SWP391_DBContext dBContext;
+
+        public DeleteModel(SWP391_DBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+        public async Task<IActionResult> OnGet(int id)
+        {
+            var category = await dBContext.Categories.Where(c => c.CategoryId== id).FirstOrDefaultAsync();
+            if(category != null)
+            {
+                dBContext.Categories.Remove(category);
+                dBContext.SaveChanges();
+            }
+            return RedirectToPage("/admin/category/index");
+        }
+    }
+}
