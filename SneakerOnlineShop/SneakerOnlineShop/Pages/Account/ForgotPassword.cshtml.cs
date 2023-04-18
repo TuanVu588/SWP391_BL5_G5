6a3ff22e61938915ad6d3f45f7dd02302420ae74
hunglmhe151273 <101168208+hunglmhe151273@users.noreply.github.com>
2023-04-18 23:34:10 +0700

    Add front-end forgot password

---
+++ SneakerOnlineShop/SneakerOnlineShop/Pages/Account/ForgotPassword.cshtml.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SneakerOnlineShop.Models;
+
+namespace SneakerOnlineShop.Pages.Account
+{
+    public class ForgotPasswordModel : PageModel
+    {
+
+        private readonly SWP391_DBContext _dbContext;
+        IMapper _mapper;
+        public double Total { get; set; }
+        [BindProperty]
+        public Customer Customer { get; set; }
+        [BindProperty]
+        public Order Order { get; set; }
+
+        public CartModel(SWP391_DBContext dBContext, IMapper mapper)
+        {
+            this._dbContext = dBContext;
+            _mapper = mapper;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
