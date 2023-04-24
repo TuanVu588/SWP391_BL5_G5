0f6ad7ae914b6171b9842f74692af6f02fecb766
hung9bge <ahungthanhgank@gmail.com>
2023-04-24 12:37:28 +0700

    update_admin_product_detail

--- SneakerOnlineShop/SneakerOnlineShop/Pages/Admin/Product/Detail.cshtml.cs
+++ SneakerOnlineShop/SneakerOnlineShop/Pages/Admin/Product/Detail.cshtml.cs
@@ -13,6 +13,7 @@ namespace SneakerOnlineShop.Pages.Admin.Product
     {
 		private readonly SWP391_DBContext dBContext;
 
+
 		public DetailModel(SWP391_DBContext dBContext)
 		{
 			this.dBContext = dBContext;
@@ -20,28 +21,39 @@ namespace SneakerOnlineShop.Pages.Admin.Product
 
 		public async Task<IActionResult> OnGet(int id)
         {
-		/*	if (HttpContext.Session.GetString("CustSession") == null)
-			{
-				return RedirectToPage("/error");
-			}
-			else
-			{
-				var account = JsonSerializer.Deserialize<MyRazorPages.Models.Account>(HttpContext.Session.GetString("CustSession"));
-				if (account.Role == 2)
-				{
-					return RedirectToPage("/error");
-				}
-			}*/
-
-			var product = await dBContext.Products.SingleOrDefaultAsync(p => p.ProductId == id);
+            /*	if (HttpContext.Session.GetString("CustSession") == null)
+                {
+                    return RedirectToPage("/error");
+                }
+                else
+                {
+                    var account = JsonSerializer.Deserialize<MyRazorPages.Models.Account>(HttpContext.Session.GetString("CustSession"));
+                    if (account.Role == 2)
+                    {
+                        return RedirectToPage("/error");
+                    }
+                }*/         
+           
+            var product = await dBContext.Products.SingleOrDefaultAsync(p => p.ProductId == id);
 			if (product == null)
 			{
+				
 				return RedirectToPage("/admin/error404");
 			}
 
 			ViewData["product"] = product;
 
-			return Page();
+            var productImage = dBContext.ProductImages.Where(p => p.ProductId == id).ToList();
+          
+            
+            if (productImage == null)
+            {
+                return RedirectToPage("/admin/error404");
+            }
+
+            ViewData["productImage"] = productImage;
+
+            return Page();
         }
     }
 }
