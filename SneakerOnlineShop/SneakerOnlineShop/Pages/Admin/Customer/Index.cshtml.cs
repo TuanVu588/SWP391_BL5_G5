a551765b9bfcbafe62895e89ed68362438e78377
TuanVu588 <80246371+TuanVu588@users.noreply.github.com>
2023-04-27 14:47:18 +0700

    update css

--- SneakerOnlineShop/SneakerOnlineShop/Pages/Admin/Customer/Index.cshtml.cs
+++ SneakerOnlineShop/SneakerOnlineShop/Pages/Admin/Customer/Index.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SneakerOnlineShop.Models;
 using System.Data;
+using System.Text.Json;
 
 namespace SneakerOnlineShop.Pages.Admin.Customer
 {
@@ -13,45 +14,63 @@ namespace SneakerOnlineShop.Pages.Admin.Customer
         private readonly SWP391_DBContext dBContext;
         public Models.Customer Customer { get; set; }
         private const int pageSize = 12;
-
+        [BindProperty]
+        public Models.Account? account { get; set; }
         public IndexModel(SWP391_DBContext dBContext)
         {
             this.dBContext = dBContext;
         }
         public async Task<IActionResult> OnGet(int numPage, string catId, string txtSearch)
         {
-            if (numPage <= 0)
-            {
-                numPage = 1;
-            }
-            var customerPerPage = await dBContext.Customers.ToListAsync();
-            var customers = await dBContext.Customers.CountAsync();
-            int totalCustomers = customers;
-            if (catId != null)
-            {
-                customerPerPage = customerPerPage.Where(p => p.CustomerId.Equals(catId)).ToList();
-            }
+            String? accountSession = HttpContext.Session.GetString("account");
 
-            if (txtSearch != null && !txtSearch.Equals(""))
+            if(accountSession is not null)
             {
-                try
+                if(account is not null && account.RoleId == 3)
                 {
-                    customerPerPage = customerPerPage.Where(p =>p.CustomerName.Contains(txtSearch)).ToList();
+                    account = JsonSerializer.Deserialize<Models.Account>(accountSession);
+                    if (numPage <= 0)
+                    {
+                        numPage = 1;
+                    }
+                    var customerPerPage = await dBContext.Customers.ToListAsync();
+                    var customers = await dBContext.Customers.CountAsync();
+                    int totalCustomers = customers;
+                    if (catId != null)
+                    {
+                        customerPerPage = customerPerPage.Where(p => p.CustomerId.Equals(catId)).ToList();
+                    }
+
+                    if (txtSearch != null && !txtSearch.Equals(""))
+                    {
+                        try
+                        {
+                            customerPerPage = customerPerPage.Where(p => p.CustomerName.Contains(txtSearch)).ToList();
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                    }
+                    customerPerPage = customerPerPage.Skip((numPage - 1) * pageSize).Take(pageSize).ToList();
+
+                    ViewData["customerPerPage"] = customerPerPage;
+                    ViewData["maxPage"] = (totalCustomers / pageSize) + (totalCustomers % pageSize == 0 ? 0 : 1);
+                    ViewData["page"] = numPage;
+                    ViewData["catId"] = catId;
+                    ViewData["txtSearch"] = txtSearch;
+                    return Page();
                 }
-                catch (Exception e)
+                else
                 {
-                    Console.WriteLine(e.Message);
+                    return RedirectToPage("/account/login");
                 }
-
             }
-            customerPerPage = customerPerPage.Skip((numPage - 1) * pageSize).Take(pageSize).ToList();
-
-            ViewData["customerPerPage"] = customerPerPage;
-            ViewData["maxPage"] = (totalCustomers / pageSize) + (totalCustomers % pageSize == 0 ? 0 : 1);
-            ViewData["page"] = numPage;
-            ViewData["catId"] = catId;
-            ViewData["txtSearch"] = txtSearch;
-            return Page();
+            else
+            {
+                return RedirectToPage("/account/login");
+            }
         }
     }
 }
